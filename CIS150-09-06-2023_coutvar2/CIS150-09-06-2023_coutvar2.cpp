81d3507d29c3bcd643099ca696e197b5d3d04ef5
KRYMZ0N <jackgrimes05@yahoo.com>
2023-09-06 10:06:17 -0400

    Add project files.

---
+++ CIS150-09-06-2023_coutvar2/CIS150-09-06-2023_coutvar2.cpp
@@ -0,0 +1,13 @@
+//==============================
+// Author: Jack Grimes
+// Date: 09-06-2023
+// Desc: Cout Var/chap2
+//==============================
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    cout << "Hello World!\n";
+}
