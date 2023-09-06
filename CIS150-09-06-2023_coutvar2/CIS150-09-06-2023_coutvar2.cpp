a49e292d06b05a4514720348c1a3cb5f89812fb2
KRYMZ0N <jackgrimes05@yahoo.com>
2023-09-06 10:48:41 -0400

    int vars

--- CIS150-09-06-2023_coutvar2/CIS150-09-06-2023_coutvar2.cpp
+++ CIS150-09-06-2023_coutvar2/CIS150-09-06-2023_coutvar2.cpp
@@ -10,4 +10,12 @@ using namespace std;
 int main()
 {
     cout << "J\na\nc\nk" << endl; //Printing name on new lines
+
+    int favNum = 10; //Definition and an assignment
+
+    int age; // Definition
+    age = 18; // assginment
+
+    cout << "Hello my name is Jack! I am " << age << " years old, and my favorite number is " << favNum << endl; //printing integer vars
+    return 0;
 }
